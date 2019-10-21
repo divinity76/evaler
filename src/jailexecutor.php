555686d671775348bd8a90475389e66865aa6d7c
hanshenrik <divinity76@gmail.com>
2019-10-21 17:51:40 +0200

    unfinished work in progress

---
+++ src/jailexecutor.php
@@ -0,0 +1,287 @@
+<?php
+declare(strict_types = 1);
+
+if (posix_geteuid() !== 0) {
+    fprintf(STDERR, "this script must run as root (only root can chroot)\n");
+    die();
+}
+if (! chdir("/jail")) {
+    fprintf(STDERR, "failed to chdir to \"\jail\" !\n");
+    die();
+}
+
+class Config
+{
+
+    // 8 megabytes, no particular reason for this default
+    public static $output_max_bytes = 8 * 1024 * 1024;
+
+    // -1 is "keep reading until EOF
+    public static $code_byte_length = - 1;
+
+    public static $runtime_max_seconds = 5;
+
+    public static $code_filename = "code.php";
+
+    public static $code_executor = "/usr/bin/php";
+
+    public static function init_from_json(string $json): void
+    {
+        $decoded = json_decode($json, true);
+        foreach ($decoded as $prop => $val) {
+            if (! property_exists("Config", $prop)) {
+                throw new \InvalidArgumentException("unknown property \"{$prop}\"");
+            }
+            if ($prop === "code_filename") {
+                if ($val !== basename($val)) {
+                    throw new \LogicException("FIXME code_filename attempted directory traversal! currently not supported..");
+                }
+            }
+            if ($prop === "code_executor") {
+                throw new \LogicException("FIXME custom code_executor currently not supported..");
+            }
+            Config::$prop = $val;
+        }
+    }
+}
+
+$protocol_version = stream_get_contents(STDIN, 1);
+if ($protocol_version !== "\x01") {
+    // ..................................................
+    die("protocol version error. supported 0x01, but got 0x" . bin2hex($protocol_version));
+}
+$config_json_length = from_little_uint32_t(stream_get_contents(STDIN, 4));
+Config::init_from_json(stream_get_contents(STDIN, $config_json_length), true);
+$code = stream_get_contents(STDIN, Config::$code_byte_length);
+if (! is_string($code)) {
+    throw new \RuntimeException('failed to read the code from stdin! (stream_get_contents failed)');
+}
+$jail_user = "jailuser" . get_jail_user();
+
+$code_file = "/jail/home/{$jail_user}/" . Config::$code_filename;
+@unlink($code_file);
+if (strlen($code) !== file_put_contents($code_file, $code, LOCK_EX)) {
+    throw new \RuntimeException('failed to write the code to disk! (out of diskspace?)');
+}
+if (! chmod($code_file, 0700)) {
+    throw new \RuntimeException('failed to chmod! (0700)');
+}
+if (! chown($code_file, $jail_user)) {
+    throw new \RuntimeException('failed to chown! (' . $jail_user . ')');
+}
+if (! chgrp($code_file, "jailgroup")) {
+    throw new \RuntimeException('failed to chgrp! (jailgroup)');
+}
+
+$starttime = microtime(true);
+$descriptorspec = array(
+    0 => array(
+        "pipe",
+        "rb"
+    ), // stdin, by default it is INHERITED from parent (us), we don't want that, so create one and close it.
+    1 => array(
+        "pipe",
+        "wb"
+    ), // stdout
+    2 => array(
+        "pipe",
+        "wb"
+    ) // stderr
+);
+$pipes = [];
+// basically: chroot --userspec=jailuser123:jailgroup /jail /bin/sh -c 'cd /home/jailuser123; /usr/bin/php code.php'
+$chroot_cmd = implode(" ", array(
+    'chroot',
+    '--userspec=' . escapeshellarg($jail_user) . ":" . escapeshellarg("jailgroup"),
+    escapeshellarg('/jail'),
+    '/bin/sh -c ' . escapeshellarg(implode(" ", array(
+        'cd ' . escapeshellarg("/home/{$jail_user}") . ";",
+        escapeshellarg(Config::$code_executor) . " " . escapeshellarg(Config::$code_filename)
+    )))
+));
+// var_dump("chroot cmd: ", $chroot_cmd) & die();
+$ph = proc_open($chroot_cmd, $descriptorspec, $pipes);
+if (false === $ph) {
+    throw new RuntimeException("failed to start chroot!  cmd: {$chroot_cmd}");
+}
+fclose($pipes[0]);
+unset($pipes[0]);
+$terminated = false; // < protection against running pkilltree() twice
+                     // OPTIMIZE ME: use stream_select() or something instead of sleep-loop
+$output_bytes = 0;
+$proxy_out = function (bool &$output_max_bytes_exceeded = null) use (&$pipes, &$output_bytes): bool {
+    $output_max_bytes_exceeded = ($output_bytes > Config::$output_max_bytes);
+    if ($output_max_bytes_exceeded) {
+        return false;
+    }
+    $max = 0xFFF;
+    $ret = false;
+    for ($i = 0; $i < $max; ++ $i) {
+        $tmp = fread($pipes[1], 8 * 1024);
+        if ($tmp === false || strlen($tmp) < 1) {
+            break;
+        }
+        // var_dump("FROM STDOUT: {$tmp}");
+        $ret = true;
+        fwrite(STDOUT, $tmp);
+        $output_bytes += strlen($tmp);
+        $output_max_bytes_exceeded = ($output_bytes > Config::$output_max_bytes);
+        if ($output_max_bytes_exceeded) {
+            return false;
+        }
+    }
+    for ($i = 0; $i < $max; ++ $i) {
+        $tmp = fread($pipes[2], 8 * 1024);
+        if ($tmp === false || strlen($tmp) < 1) {
+            break;
+        }
+        // var_dump("FROM STDERR: {$tmp}");
+        $ret = true;
+        fwrite(STDERR, $tmp);
+        $output_bytes += strlen($tmp);
+        $output_max_bytes_exceeded = ($output_bytes > Config::$output_max_bytes);
+        if ($output_max_bytes_exceeded) {
+            return false;
+        }
+    }
+    return $ret;
+};
+
+$output_max_bytes_exceeded = false;
+while (($status = proc_get_status($ph))['running']) {
+    usleep(100 * 1000); // *1000 = ms
+    if (! $terminated && ((microtime(true) - $starttime) > Config::$runtime_max_seconds)) {
+        $terminated = true;
+        fwrite(STDERR, PHP_EOL . 'max runtime reached (' . (Config::$runtime_max_seconds) . ' seconds), terminating...');
+        pkilltree((int) ($status['pid']));
+        // proc_terminate ( $ph, SIGKILL );
+    }
+    $proxy_out($output_max_bytes_exceeded);
+    if (! $terminated && $output_max_bytes_exceeded) {
+        $terminated = true;
+        pkilltree((int) ($status['pid']));
+        // proc_terminate ( $ph, SIGKILL );
+        fwrite(STDERR, PHP_EOL . 'max output bytes exceeded! (' . ((string) Config::$output_max_bytes) . "), terminating...");
+    }
+}
+if (! $terminated) {
+    // make sure descendants are killed, like in North Korea
+    pkilltree((int) ($status['pid']));
+}
+while (false !== $proxy_out($output_max_bytes_exceeded));
+if ($output_max_bytes_exceeded) {
+    fwrite(STDERR, PHP_EOL . 'max output bytes exceeded! (' . ((string) Config::$output_max_bytes) . "), output probably truncated...");
+}
+// echo "\nexit status: " . $status['exitcode'];
+proc_close($ph);
+// now to clean up..
+// find "/jail/home/jailuser123/" -mindepth 1 -delete | find /jail/tmp -user jailuser123 -delete
+shell_exec("find " . escapeshellarg("/jail/home/{$jail_user}/") . " -mindepth 1 -delete | find " . escapeshellarg("/jail/tmp") . " -user " . escapeshellarg($jail_user) . " -delete");
+
+exit($status['exitcode']);
+
+// ...
+function strsignal(int $signo): ?string
+{
+    foreach (get_defined_constants(true)['pcntl'] as $name => $num) {
+        // the _ is to ignore SIG_IGN and SIG_DFL and SIG_ERR and SIG_BLOCK and SIG_UNBLOCK and SIG_SETMARK, and maybe more, who knows
+        if ($num === $signo && substr($name, 0, 3) === "SIG" && $name[3] !== "_") {
+            return $name;
+        }
+    }
+    return null;
+}
+
+function get_jail_user(): int
+{
+    // TODO: rewrite it in SQLite + PDO rather than manual json db...
+    static $cached = NULL;
+    if ($cached !== NULL) {
+        return $cached;
+    }
+    if (false === ($fp = fopen("jailaccounts.json", "r+b"))) {
+        throw new \RuntimeException("failed to open jailaccounts.json!");
+    }
+    while (true) {
+        if (true !== flock($fp, LOCK_EX)) {
+            throw new \RuntimeException("failed to flock jaildb.txt! (LOCK_EX)");
+        }
+        $accounts = json_decode(stream_get_contents($fp), true);
+        $found = null;
+        foreach ($accounts as $accountnumber => $data) {
+            if (! $data['busy']) {
+                $found = $accountnumber;
+                $cached = $found;
+                break;
+            }
+        }
+        if ($found !== null) {
+            break;
+        } else {
+            // dammit, all accounts are busy, need to wait...
+            error_log("all jail accounts are busy! need to wait... number of jail accounts: " . ((string) count($accounts)));
+            flock($fp, LOCK_UN);
+            rewind($fp);
+            sleep(2); // dunno how long is sensible to wait before checking again...
+            continue; // re-check if any acounts are available after finished sleeping..
+        }
+    }
+    $accounts[$found]['busy'] = true;
+    $accounts[$found]['since'] = date(DateTime::ATOM);
+    rewind($fp);
+    $new_data = json_encode($accounts);
+    fwrite($fp, $new_data);
+    ftruncate($fp, strlen($new_data));
+    flock($fp, LOCK_UN);
+    fclose($fp);
+    unset($new_data);
+    register_shutdown_function(function () use (&$cached) {
+        if (false === ($fp = fopen("jailaccounts.json", "r+b"))) {
+            throw new \RuntimeException("failed to open jailaccounts.json!");
+        }
+        if (true !== flock($fp, LOCK_EX)) {
+            throw new \RuntimeException("failed to flock jaildb.txt! (LOCK_EX)");
+        }
+        $accounts = json_decode(stream_get_contents($fp), true);
+        assert($accounts[$cached]['busy'] === true, "the account we assinged to ourself must be busy!");
+        $accounts[$cached]['busy'] = false;
+        $accounts[$cached]['since'] = date(DateTime::ATOM);
+        rewind($fp);
+        $new_data = json_encode($accounts);
+        fwrite($fp, $new_data);
+        ftruncate($fp, strlen($new_data));
+        unset($new_data);
+        flock($fp, LOCK_UN);
+        fclose($fp);
+    });
+    return $cached;
+}
+
+function pkilltree(int $pid)
+{
+    shell_exec("kill -s STOP " . $pid . " 2>&1"); // stop it first, so it can't make any more children
+    $children = shell_exec('pgrep -P ' . $pid);
+    if (is_string($children)) {
+        $children = trim($children);
+    }
+    if (! empty($children)) {
+        $children = array_filter(array_map('trim', explode("\n", $children)), function ($in) {
+            return false !== filter_var($in, FILTER_VALIDATE_INT); // shouldn't be necessary, but just to be safe..
+        });
+        foreach ($children as $child) {
+            pkilltree((int) $child);
+        }
+    }
+    shell_exec("kill -s KILL " . $pid . " 2>&1");
+}
+
+function to_little_uint32_t(int $i): string
+{
+    return pack('V', $i);
+}
+
+function from_little_uint32_t(string $i): int
+{
+    $arr = unpack('Vuint32_t', $i);
+    return $arr['uint32_t'];
+}
---
+++ src/setup.php
@@ -0,0 +1,224 @@
+#!/usr/bin/env php
+<?php
+declare(strict_types = 1);
+init();
+
+const JAIL_USERS = 100;
+/** @var int $argc */
+/** @var string[] $argv */
+
+if (is_dir("/jail") || ($argc >= 2 && $argv[1] == "--uninstall")) {
+    echo "removing pre-existing installation...";
+    $commands = array(
+        'umount /jail/bin',
+        'umount /jail/lib',
+        'umount /jail/lib64',
+        'umount /jail/usr',
+        'umount /jail/etc/alternatives',
+        'umount /jail/etc/php',
+        // 'umount /jail/etc/',
+        // 'umount /jail/dev/pts',
+        // 'umount /jail/dev',
+        // 'sleep 100',
+        'rm -rf /jail'
+    );
+    foreach ($commands as $command) {
+        echo "executing: \"{$command}\".. ";
+        $stdout = $stderr = "";
+        $ret = my_shell_exec($command, "", $stdout, $stderr);
+        if ($ret === 0 && empty($stdout) && empty($stderr)) {
+            echo "ok.\n";
+        } else {
+            echo "{$ret} stdout:" . (return_var_dump($stdout)) . " - stderr: " . (return_var_dump($stdout)) . "\n";
+        }
+    }
+    if (0) {
+        echo "deleting jailuser{1-" . JAIL_USERS . "}\n";
+        for ($i = 1; $i <= JAIL_USERS; ++ $i) {
+            shell_exec("deluser jailuser{$i}");
+        }
+    }
+    if (file_exists('/jail')) {
+        throw new \LogicException('FAILED TO DELETE /jail');
+    }
+    echo "uninstall complete.\n";
+    if ($argc >= 2 && $argv[1] == "--uninstall") {
+        echo "thanks to --uninstall , that's all folks\n";
+        exit(0);
+    }
+}
+
+$commands = <<<'COMMANDS'
+mkdir /jail /jail/bin /jail/lib /jail/lib64 /jail/usr /jail/etc /jail/etc/alternatives /jail/home /jail/tmp
+#mkdir /jail/dev
+mkdir /jail/etc/php
+#everybody can execute
+chmod -R 0711 /jail
+#everybody can read and execute
+chmod -R 0755 /jail/etc/php
+# everybody can execute and write (but not read)
+chmod -R 0733 /jail/tmp
+chown -R root:root /jail
+#mount -o bind,ro /dev /jail/dev
+#mount -o bind,ro /dev/pts /jail/dev/pts
+mount -o bind,ro /bin /jail/bin
+mount -o bind,ro /lib /jail/lib
+mount -o bind,ro /lib64 /jail/lib64
+mount -o bind,ro /usr /jail/usr
+#mount -o bind,ro /etc/ /jail/etc/
+mount -o bind,ro /etc/php /jail/etc/php
+mount -o bind,ro /etc/alternatives /jail/etc/alternatives
+#adduser jailgroup
+COMMANDS;
+$commands = array_filter(array_map("trim", explode("\n", $commands)), function (string $str) {
+    return ! (strlen($str) <= 0 || $str[0] === "#");
+});
+
+foreach ($commands as $command) {
+    echo "executing: \"{$command}\".. ";
+    $stdout = $stderr = "";
+    $ret = my_shell_exec($command, "", $stdout, $stderr);
+    if ($ret === 0 && empty($stdout) && empty($stderr)) {
+        echo "ok.\n";
+    } else {
+        echo "{$ret} stdout: " . (return_var_dump($stdout)) . " - stderr: " . (return_var_dump($stderr)) . "\n";
+    }
+}
+my_shell_exec("addgroup jailgroup");
+echo "creating jailuser{0-" . JAIL_USERS . "}..";
+for ($i = 1; $i <= JAIL_USERS; ++ $i) {
+    // useradd --home /jailuser1 --shell /bin/false --gid jailgroup -M jailuser1
+    shell_exec("useradd --home /home/jailuser{$i} --shell /bin/false --gid jailgroup -M jailuser{$i} 2>&1");
+    mkdir("/jail/home/jailuser{$i}", 0700);
+    chown("/jail/home/jailuser{$i}", "jailuser{$i}");
+    chgrp("/jail/home/jailuser{$i}", "jailgroup");
+}
+echo "done!\n";
+if (false) {
+    // TODO: rewrite it in SQLite instead of json
+    echo "now creating jailaccounts.db3..";
+    call_user_func(function () {
+        if (is_file("/jail/jailaccounts.db3")) {
+            unlink("/jail/jailaccounts.db3");
+        }
+        $db = new \PDO('sqlite:/jail/jailaccounts.db3', '', '', array(
+            \PDO::ATTR_EMULATE_PREPARES => false,
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+        ));
+        $db->exec("CREATE TABLE jailaccounts (id INTEGER PRIMARY KEY, busy TINYINT(1), since TEXT);");
+        $db->beginTransaction();
+        $stm = $db->prepare("INSERT INTO jailaccounts (id,busy,since) VALUES(?,?,?);");
+        for ($i = 1; $i <= JAIL_USERS; ++ $i) {
+            $stm->execute(array(
+                $i,
+                0,
+                date(DateTime::ATOM, 0) // 1970-01-01
+            ));
+        }
+        $db->commit();
+    });
+    echo " done.\n";
+}
+echo "now creating jailaccounts.json..";
+$db = [];
+for ($i = 1; $i <= JAIL_USERS; ++ $i) {
+    $db[$i] = [
+        "busy" => false,
+        "since" => date(DateTime::ATOM, 0)
+    ];
+}
+file_put_contents("/jail/jailaccounts.json", json_encode($db), LOCK_EX);
+echo " done.\n";
+file_put_contents("/etc/sudoers.d/www-data-jailexecutor", "www-data ALL = (root) NOPASSWD: /usr/bin/php /jail/jailexecutor.php", LOCK_EX);
+echo "setup complete!\n";
+echo "next up, add this to /etc/sudoers: www-data ALL = (root) NOPASSWD: /usr/bin/php /jail/jailexecutor.php\n";
+
+function return_var_dump(): string
+{
+    $args = func_get_args();
+    ob_start();
+    call_user_func_array('var_dump', $args);
+    return ob_get_clean();
+}
+
+function my_shell_exec(string $cmd, string $stdin = null, string &$stdout = null, string &$stderr = null): int
+{
+    // echo "executing \"{$cmd}\"...";
+    // use a tmpfile in case stdout is so large that the pipe gets full before we read it, which would result in a deadlock.
+    $stdout_handle = tmpfile();
+    $stderr_handle = tmpfile();
+    $descriptorspec = array(
+        // stdin is *inherited* by default, so even if $stdin is empty, we should create a stdin pipe just so we can close it.
+        0 => array(
+            "pipe",
+            "rb"
+        ),
+        1 => $stdout_handle,
+        2 => $stderr_handle
+    );
+    $pipes = [];
+    $proc = proc_open($cmd, $descriptorspec, $pipes);
+    if (! $proc) {
+        throw \RuntimeException("proc_exec failed!");
+    }
+
+    // TODO: fwrite returns < strlen($stdin) ?
+    if (! is_null($stdin) && strlen($stdin) > 0) {
+        fwrite($pipes[0], $stdin);
+    }
+    fclose($pipes[0]);
+    $ret = proc_close($proc);
+    rewind($stdout_handle); // stream_get_contents can seek but it has let me down earlier, https://bugs.php.net/bug.php?id=76268
+    rewind($stderr_handle); //
+    $stdout = stream_get_contents($stdout_handle);
+    fclose($stdout_handle);
+    $stderr = stream_get_contents($stderr_handle);
+    fclose($stderr_handle);
+    // echo "done!\n";
+    return $ret;
+}
+
+function init()
+{
+    if (posix_getuid() !== 0) {
+        echo ("only root can run this script!\n");
+        exit(1);
+    }
+    hhb_init();
+}
+
+function hhb_init()
+{
+    static $firstrun = true;
+    if ($firstrun !== true) {
+        return;
+    }
+    $firstrun = false;
+    error_reporting(E_ALL);
+    set_error_handler("hhb_exception_error_handler");
+    // ini_set("log_errors",'On');
+    // ini_set("display_errors",'On');
+    // ini_set("log_errors_max_len",'0');
+    // ini_set("error_prepend_string",'<error>');
+    // ini_set("error_append_string",'</error>'.PHP_EOL);
+    // ini_set("error_log",__DIR__.DIRECTORY_SEPARATOR.'error_log.php.txt');
+    assert_options(ASSERT_ACTIVE, 1);
+    assert_options(ASSERT_WARNING, 0);
+    assert_options(ASSERT_QUIET_EVAL, 1);
+    assert_options(ASSERT_CALLBACK, 'hhb_assert_handler');
+}
+
+function hhb_exception_error_handler($errno, $errstr, $errfile, $errline)
+{
+    if (! (error_reporting() & $errno)) {
+        // This error code is not included in error_reporting
+        return;
+    }
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+
+function hhb_assert_handler($file, $line, $code, $desc = null)
+{
+    $errstr = 'Assertion failed at ' . $file . ':' . $line . ' ' . $desc . ' code: ' . $code;
+    throw new ErrorException($errstr, 0, 1, $file, $line);
+}
